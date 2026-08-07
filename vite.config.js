d2c2f9f8166b99897ff91520e536bc12656af835
Soumik Piratla <soumpirat@gmail.com>
2026-08-07 10:59:03 +0530

    more minor

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/eHCV-TCO-India/', // Make sure to keep the surrounding slashes
+  base: '/eHCV-TCOv2/', // Make sure to keep the surrounding slashes
 })
